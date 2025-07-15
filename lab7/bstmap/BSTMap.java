da18bc8efda611e68e6b94351392abb44ee8f0fc
roger <miracle_0824@qq.com>
2025-07-15 17:04:59 +0800

    remove v1

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -94,24 +94,53 @@ public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V> {
         throw new UnsupportedOperationException("keySet not support");
     }
 
+    // remove problem has 3 cases:
+    // 1\ Key with no children
+    // 2\ Key with 1 child
+    // 3\ Key with 2 children
 
     public V remove(K key) {
-        throw new UnsupportedOperationException("remove not support");
-//        if(key == null) throw new IllegalArgumentException("calls remove() with a null key");
-//        if(!containsKey(key)) return null;
-//
+//        throw new UnsupportedOperationException("remove not support");
+        if(key == null) throw new IllegalArgumentException("calls remove() with a null key");
+        if(!containsKey(key)) return null;
 
+        V removedVal = get(key);
+        this.root = remove(this.root, key);
+        return removedVal;
     }
-//
-//    private BSTNode remove(BSTNode x, K key) {
-//        if(x == null) return null;
-//
-//        int cmp = x.key.compareTo(key);
-//
-//    }
+
+    private BSTNode remove(BSTNode x, K key) {
+        if(x == null) return null;
+
+        int cmp = x.key.compareTo(key);
+        // In-order traversal to find the goal
+        if(cmp < 0) x.right = remove(x.right, key);
+        else if (cmp > 0) x.left = remove(x.left, key);
+        else {
+            // case 1 && 2(no left child)
+            if(x.left == null) return x.right;
+            // case 2(no right child)
+            if(x.right == null) return x.left;
+
+            // case 3: has two children  (algorithm: Hibbard deletion)
+            BSTNode successor = findMin(x.right);
+            x.value = successor.value;
+            x.key = successor.key;
+            x.right = remove(x.right, successor.key); // remove the successor
+        }
+        return x;
+    }
 
     public V remove(K key, V value) {
         throw new UnsupportedOperationException("remove not support");
+//        if(key == null) throw new IllegalArgumentException("calls remove() with a null key");
+//        if(!containsKey(key)) return null;
+//        if(get(key) != value) throw new IllegalArgumentException("calls remove() with a wrong key-value");
+    }
+
+    private BSTNode findMin(BSTNode x) {
+        while(x.left != null) x = x.left;
+        return x;
     }
 
     public Iterator<K> iterator() {
@@ -131,17 +160,18 @@ public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V> {
         printInOrder(x.right);
     }
 
-//    public static void main(String args[]) {
-//        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
-//        b.put("bee",2);
-//        b.put("ant", 3);
-//        b.put("cat",1);
-//        b.put("dog",8);
-//        b.printInOrder();
-//        Integer x = b.remove("cat");
-//        b.printInOrder();
-//        System.out.println(x);
-//    }
+    public static void main(String args[]) {
+        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        b.put("bee",2);
+        b.put("ant", 3);
+        b.put("cat",1);
+        b.put("dog",8);
+        b.printInOrder();
+        Integer x = b.remove("cat");
+        b.printInOrder();
+        System.out.println(b.size());
+        System.out.println(x);
+    }
 
 
 
